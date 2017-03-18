f25f5b7b3f77b7093267edee02fb45ba560c96f7
WoodA7 <wood_a7@mail.ru>
2017-03-18 11:51:23 +0200

    Ver 1

--- Chat/MainWindow.xaml.cs
+++ Chat/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Helper;
 
 namespace Chat
 {
@@ -23,6 +27,26 @@ namespace Chat
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            var richText = new TextRange(rtbMessage.Document.ContentStart, rtbMessage.Document.ContentEnd).Text;
+            var from = "Dmitri";
+            var msg = new Message(richText, from, "", DateTime.Now);
+
+            try
+            {
+                var tcpClient = new TcpClient();
+                tcpClient.Connect($"192.168.2.94", 4444);
+                IFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(tcpClient.GetStream(), msg);
+                rtbMessage.Document.Blocks.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
